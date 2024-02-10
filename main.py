73fdeedbdb72ebf3ed03de52257ed76e9ad097a4
chantroi <tonglanhchua@gmail.com>
2024-02-10 15:07:41 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 from hydrogram import Client, filters
-from environ import api_id, api_hash, bot_token, google_api
-from preset import generation_config, safety_settings
+from environ import api_id, api_hash, bot_token
 from hydrogram.enums import ChatAction
-import google.generativeai as genai
+from res import pro, pro_vision
 import os
 
 app = Client("Google AI", api_id, api_hash, bot_token=bot_token, in_memory=True)
-genai.configure(api_key=google_api)
-model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config, safety_settings=safety_settings)
-chat = model.start_chat(history=[
-])
 
 @app.on_message(filters.command(["start","help"]) & filters.private)
 async def basic(c, m):
     await m.reply_chat_action(ChatAction.TYPING)
     await m.reply("Hello, __--Welcome to Unofficial Google AI--__", quote=True)
     
-@app.on_message(filters.mentioned | filters.private)
-async def chatting(c, m):
+@app.on_message((filters.mentioned | filters.private) & filters.text)
+async def pro_model(c, m):
     if m.text.startswith("@"):
         text = m.text.split(m.command[0])
     else:
         text = m.text
+    res = pro(text)
     await m.reply_chat_action(ChatAction.TYPING)
-    res = chat.send_message(text)
-    await m.reply(res.text, quote=True)
+    await m.reply(res, quote=True)
+    
+@app.on_message((filters.mentioned | filters.private) & filters.photo)
+async def pro_vision_model(c, m):
+    try:
+        if m.reply_to_message:
+            text = m.reply_to_message.caption
+            photo = await c.download_media(m.reply_to_message, in_memory=True)
+        else:
+            raise
+    except Exception as e:
+        print(e)
+        text = m.caption
+        photo = await c.download_media(m, in_memory=True)
+    if text.startswith("@"):
+        text = text.replace(m.command[0], "")
+    else:
+        text = m.text
+    res = pro_vision(photo.getvalue(), text)
+    await m.reply_chat_action(ChatAction.TYPING)
+    await m.reply(res, quote=True)
     
 app.run()
