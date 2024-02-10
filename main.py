02c83737f609fcbabdc6dba7b79915e9ae7685bc
chantroi <tonglanhchua@gmail.com>
2024-02-10 14:08:04 +0700

    update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 app = Client("Google AI", api_id, api_hash, bot_token=bot_token, in_memory=True)
 genai.configure(api_key=google_api)
-model = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config, safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config, safety_settings=safety_settings)
 chat = model.start_chat(history=[
 ])
 
