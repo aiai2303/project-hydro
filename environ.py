7477034d09e97aa0bdbb93d3c09046d04802f7e5
chantroi <tonglanhchua@gmail.com>
2024-02-10 09:05:37 +0700

    update environ.py

--- environ.py
+++ environ.py
@@ -5,5 +5,5 @@ SECRET = os.getenv("SECRET")
 res = requests.get(SECRET).json()
 api_id = res["key"]["api_id"]
 api_hash = res["key"]["api_hash"]
-bot_token = res["bot"]["AI"]
+bot_token = res["bot"]["ai_tg"]
 google_api = res["key"]["google_ai"]
