5acf3d6b0546f3ae9c0b270453e74b133133c27a
chantroi <tonglanhchua@gmail.com>
2024-02-10 15:20:33 +0700

    update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def pro_vision_model(c, m):
         text = text.replace(m.command[0], "")
     else:
         text = m.text
-    res = pro_vision(photo.getvalue(), text)
+    res = pro_vision(photo, text)
     await m.reply_chat_action(ChatAction.TYPING)
     await m.reply(res, quote=True)
     
