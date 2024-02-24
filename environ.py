62ff3c4047246f6ea01f4a68cd77893c7d10f305
chantroi <tonglanhchua@gmail.com>
2024-02-24 18:33:23 +0700

    update environ.py

--- environ.py
+++ environ.py
@@ -2,8 +2,7 @@ import requests
 import os
 
 SECRET = os.getenv("SECRET")
+TELECAST = os.getenv("TELECAST")
 res = requests.get(SECRET).json()
-api_id = res["key"]["api_id"]
-api_hash = res["key"]["api_hash"]
-bot_token = res["bot"]["ai_tg"]
 google_api = res["key"]["google_ai"]
+session = requests.get(TELECAST).text
