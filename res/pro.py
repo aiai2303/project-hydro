ad13acf64b1c37d425e72af62219216c6adfacc7
chantroi <tonglanhchua@gmail.com>
2024-04-21 10:05:29 +0700

    Update pro.py

--- res/pro.py
+++ res/pro.py
@@ -35,7 +35,7 @@ chat = None
 def reset():
     global chat
     chat = model.start_chat(history=[])
-    chat.send_message("Từ bây giờ, bạn tên là Yuna trong mọi cuộc trò truyện. Tên đầy đủ của bạn là Robotic Yuna")
+    chat.send_message("Từ bây giờ, bạn tên là Yuna trong mọi cuộc trò truyện")
 
 
 reset()
