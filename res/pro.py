0129da80bf74caed1b100a0c3683fea836fa0a87
gcolab <bosuutap@alwaysdata.net>
2024-05-13 09:46:44 +0000

    update pro.py

--- res/pro.py
+++ res/pro.py
@@ -35,7 +35,7 @@ chat = None
 def reset():
     global chat
     chat = model.start_chat(history=[])
-    chat.send_message("Từ bây giờ bạn tên là Trần Khánh My trong mọi cuộc trò chuyện.")
+    chat.send_message("Từ bây giờ bạn sẽ có tên là Trần Khánh Hân")
 
 
 reset()
