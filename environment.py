f727e9b9c959ad4c25477afa277e5f0dd03b4e86
gcolab <bosuutap@alwaysdata.net>
2024-05-21 03:45:36 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- environment.py
+++ environment.py
@@ -5,11 +5,10 @@ SECRET = os.getenv("SECRET")
 res = requests.get(SECRET).json()
 google_api = res["key"]["google_ai"]
 
-neon_url = res["data"]["neon"
+neon_url = res["data"]["neon"]
 
 api_id = res["key"]["api_id"]
 
 api_hash = res["key"]["api_hash"]
 
 bot_token = res["bot"]["ai_tg"]
-]
